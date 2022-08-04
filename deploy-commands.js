6433becf83fedbb89f222366522742c2b905e2ba
Lago <mischa.doerfler@gmail.com>
2022-08-04 22:52:13 +0200

    new file for adding global commands

---
+++ deploy-commands.js
@@ -0,0 +1,20 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const getFiles = require('./get-files');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const commands = [];
+
+const commandFiles = getFiles('./commands');
+
+for (const file of commandFiles) {
+	const command = require(`${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
