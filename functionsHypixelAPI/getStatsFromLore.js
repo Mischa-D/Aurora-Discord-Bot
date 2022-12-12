620c816cd6b90021ceef372788856f84af3bac25
Lago <mischa.doerfler@gmail.com>
2022-12-12 15:06:25 +0100

    fixed getItemStats only retrieving int values

--- functionsHypixelAPI/getStatsFromLore.js
+++ functionsHypixelAPI/getStatsFromLore.js
@@ -1,6 +1,6 @@
 async function getItemStats(item) {
 	const stats = {};
-	const statRegexp = /^([A-Z][A-Za-z\s]+):\s\+([\d+,?\d+]+\b)/gm;
+	const statRegexp = /^([A-Z][A-Za-z\s]+):\s\+([\d+,?\d+.?\d]+\b)/gm;
 	const matches = item.lore.matchAll(statRegexp);
 	for (const match of matches) {
 		stats[match[1]] = Number(match[2].split(',').join(''));
